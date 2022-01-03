4778455d4dad5a2542921145a72c653f23449075
Kourken <kkasbarian@outlook.com>
2022-01-04 02:44:30 +0400

    Updated Countdown Title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import './countdown-styles.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='countdown-container'>
+      <Countdown />
+    </div>
+  );
+}
+
+const Countdown = () => {
+
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+
+    const difference = +new Date(`01/01/${year + 1}`) - +new Date();
+    
+    let timeLeft = {};
+
+    if (difference > 0) {
+      let dayFormatting = Math.floor(difference / (1000 * 60 * 60 * 24));
+      timeLeft = {
+        days: (dayFormatting < 100) ? ('0' + dayFormatting).slice(-2) : dayFormatting,
+        hours: ('0' + Math.floor((difference / (1000 * 60 * 60)) % 24 ) ).slice(-2),
+        minutes: ('0' + Math.floor((difference / 1000 / 60) % 60 ) ).slice(-2),
+        seconds: ('0' + Math.floor((difference / 1000) % 60) ).slice(-2)
+      };
+    }
+
+    return timeLeft;
+  }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if ( !timeLeft[interval] ) {
+      timerComponents.push(
+        <div className='interval-container'>
+          <span key={interval} className='interval-num'>
+            {timeLeft[interval]}
+          </span><br></br>
+          <span className='interval-name'>{interval}</span>
+        </div>
+      );
+    } else {
+      timerComponents.push(
+        <div className='interval-container'>
+          <span key={interval} className='interval-num'>
+            {timeLeft[interval]}
+          </span><br></br>
+          <span className='interval-name'>{interval}</span>
+        </div>
+      );
+    }
+
+  });
+
+
+  return (
+    <div className='countdown-content'>
+      <h2 className='countdown-title'>2023 Countdown</h2>
+      {timerComponents.length ? timerComponents : <div className='interval-container interval-finished'><span>Happy New Year</span></div>}
     </div>
   );
 }
